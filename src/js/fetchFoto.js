6346322a02a667754e8b40808e31734b5ea85513
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-06-16 18:33:22 +0300

    on two js.fail

--- src/js/fetchFoto.js
+++ src/js/fetchFoto.js
@@ -1,26 +1,22 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-export { getFoto };
-    const galleryRefs = document.querySelector('.gallery')
-const $Form = document.querySelector('#search-form');
-const $Input = document.querySelector('input[name=searchQuery]');
-const $Button = document.querySelector('button')
-$Form.addEventListener('submit', getFoto)
-const myKey = '28048409-7c5d239fb0980a21fe8515423';
-const baseURL = "https://pixabay.com/api/";
-
-async function getFoto(e) {
-    e.preventDefault()
-    const text = e.target.searchQuery.value;
-
-      const response = await axios.get(`${baseURL}`, {params: {
-    key: myKey,
-    q: text,
-    image_type: 'foto',
-    orientation: 'horizontal',
-    safesearch: true,
-      }
-      });
+export default class GetPhotoApi {
+  constructor() {
+    this.searchValue = '';
+    this.page = 1;
+  }
+  async getPhoto() {
+    const url = 'https://pixabay.com/api/';
+const searchParams = new URLSearchParams({
+  key: '28048409-7c5d239fb0980a21fe8515423',
+  q: this.searchValue,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: this.page,
+  per_page: 40,
+});
+      const response = await axios.get(`${url}?${searchParams}`);
     console.log(response);
     //   if (!response.ok) {
     //   throw new Error(
@@ -32,37 +28,44 @@ async function getFoto(e) {
     console.log(data);
     const hits = await data.hits
     console.log(hits);
-    renderSearchFoto(hits);
     // return hits;
 }
-
-// getFoto().then(hits => {
-// renderSearchFoto(hits)
-// }
-// )
-    
-function renderSearchFoto(response) {
-  const markUpList = response
-    .map(
-      el =>
-        `<div class="photo-card">
-  <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${el.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${el.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${el.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${el.downloads}</b>
-    </p>
-  </div>
-</div>`
-    )
-    .join("");
-  galleryRefs.innerHTML = markUpList;
 }
+// export { getFoto };
+//     const galleryRefs = document.querySelector('.gallery')
+// const $Form = document.querySelector('#search-form');
+// const $Input = document.querySelector('input[name=searchQuery]');
+// const $Button = document.querySelector('button')
+// $Form.addEventListener('submit', getFoto)
+// const myKey = '28048409-7c5d239fb0980a21fe8515423';
+// const baseURL = "https://pixabay.com/api/";
+
+// async function getFoto(e) {
+//     e.preventDefault()
+//     const text = e.target.searchQuery.value;
+
+//       const response = await axios.get(`${baseURL}`, {params: {
+//     key: myKey,
+//     q: text,
+//     image_type: 'foto',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//       }
+//       });
+//     console.log(response);
+//     //   if (!response.ok) {
+//     //   throw new Error(
+//     //     Notiflix.Notify.failure('Oops, there is no foto with that name')
+//     //   );
+//     // } 
+//     // return response;
+//     const data = await response.data
+//     console.log(data);
+//     const hits = await data.hits
+//     console.log(hits);
+//     renderSearchFoto(hits);
+//     // return hits;
+// }
+
+
+    
--- src/js/project.js
+++ src/js/project.js
@@ -1,23 +1,21 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-import { getFoto } from "./fetchFoto";
+import GetPhotoApi from "./fetchFoto";
+const getPhotoApi = new GetPhotoApi();
 const galleryRefs = document.querySelector('.gallery')
 const $Form = document.querySelector('#search-form');
 const $Input = document.querySelector('input[name=searchQuery]');
-// const $Button = document.querySelector('button')
-// $Form.addEventListener('submit', submitHandler)
+const $Button = document.querySelector('button')
+$Form.addEventListener('submit', submitHandler)
 // const myKey = '28048409-7c5d239fb0980a21fe8515423';
 // const baseURL = "https://pixabay.com/api/";
-getFoto
-// function submitHandler(e) {
-//     e.preventDefault()
-//     const text = e.target.searchQuery.value;
-//     getFoto()
-//         .then(response => renderSearchFoto(response.hits))
-
-//         .catch(error =>
-//             console.error(error));
-// }
+// getFoto
+function submitHandler(e) {
+    e.preventDefault()
+    getPhotoApi.searchValue = e.target.searchQuery.value;
+  getPhotoApi.getPhoto();
+        
+}
 
 function renderSearchFoto(response) {
   const markUpList = response
